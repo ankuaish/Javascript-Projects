5556ec3341cbbcd5943c02cd92f37c59b1c115f5
ankuaish <anku.kesharwani17@gmail.com>
2024-05-01 02:26:20 +0530

    projects

---
+++ Weather/pract.js
@@ -0,0 +1,25 @@
+let dt = 1708667988;
+const curDate = new Date( dt*1000);
+// console.log(curDate);
+
+const options = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    //   second: "numeric",
+  };
+  
+  const formatter = new Intl.DateTimeFormat("en-US", options);
+  // console.log(formatter);
+  const formattedDate = formatter.format(curDate);
+  console.log(formattedDate);
+  
+  return formattedDate;
+
+
+  let num = 3232.552;
+console.log(num.toFixed());
+console.log(Math.round(num));
